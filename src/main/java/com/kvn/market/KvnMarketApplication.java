5cf7f3e4cd215dd36ad1f1221d8af228503b1566
Kevin <caballero.kevin94@gmail.com>
2021-08-06 16:06:56 +0200

    Initial commit
    initial commit

---
+++ src/main/java/com/kvn/market/KvnMarketApplication.java
@@ -0,0 +1,13 @@
+package com.kvn.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KvnMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KvnMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kvn/market/KvnMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kvn.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KvnMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
